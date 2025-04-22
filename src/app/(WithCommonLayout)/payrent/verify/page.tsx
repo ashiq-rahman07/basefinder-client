c6c8aaed4981677868fe864d9892c83549286c87
ashiq-rahman07 <web3.0.ashiq@gmail.com>
2025-04-22 20:24:18 +0600

    eslint configure bug fix

--- src/app/(WithCommonLayout)/payrent/verify/page.tsx
+++ src/app/(WithCommonLayout)/payrent/verify/page.tsx
@@ -1,66 +1,4 @@
-// "use client"
 
-// import PaymentStatus from "@/components/modules/payment";
-// import { Button } from "@/components/ui/button";
-// import { getSinglePayment, verifyPayment } from "@/services/payment";
-// import { IPayment } from "@/types/payment";
-
-// import {  useSearchParams } from "next/navigation";
-// import { useEffect, useState } from "react";
-// import { toast } from "sonner";
-
-// const PaymentVerifyPage = () => {
-//   const searchParams = useSearchParams();
-//   const order_id = searchParams.get('order_id') as string;
-//   const [paymentInfo ,setPaymentInfo] = useState<IPayment>()
-
-//   console.log(order_id);
-
-// console.log(paymentInfo);
-//   useEffect(()=>{
-//       const fetchData = async () => {
-//         const [paymentData] = await Promise.all([
-//           getSinglePayment(order_id),
-
-//         ]);
-
-//         setPaymentInfo(paymentData?.data);
-
-//       };
-
-//       fetchData();
-//   }, [])
-
-//   const handlePayment = async() => {
-
-//     const verifyPaymentData = await verifyPayment(order_id)
-
-//     if(verifyPaymentData.success){
-//       setPaymentInfo(verifyPaymentData)
-
-//         toast.success('Payment verify succesfully')
-
-//     }
-
-//     toast.success('')
-// }
-//   return (
-//   <div className="mt-20">
-//   {
-//     paymentInfo &&  <PaymentStatus data={paymentInfo}/>
-//   }
-//   {
-//     paymentInfo?.status =='Pending' && <div>
-//       <p>Verify payment for Rent this listing</p>
-//     <Button onClick={handlePayment}>Verify Payment</Button>
-//     </div>
-//   }
-
-//   </div>
-//   )
-// }
-
-// export default PaymentVerifyPage
 
 'use client';
 
@@ -90,7 +28,7 @@ const PaymentSuccessPage = () => {
     };
 
     fetchData();
-  }, []);
+  }, [order_id]);
   const handleVerify = async () => {
     try {
       setVerifying(true);
